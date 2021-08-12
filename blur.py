3cec82debf1f2cecc201e0b7c7b557dda0fab543
ltx <freez310029@gmail.com>
2021-08-12 17:32:58 +0800

    blur

---
+++ blur.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('test.jpg')
+blur = cv.blur(img,(5,5))
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+blur = cv.GaussianBlur(img,(5,5),0)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
